ea378e138541ed726d56a0b5b2583822ab50c399
AmithshaS <amithsha92@gmail.com>
2023-08-14 18:14:44 +0530

    added new methods to write to directory

--- src/main/java/org/example/SQLDatabaseConnection.java
+++ src/main/java/org/example/SQLDatabaseConnection.java
@@ -20,6 +20,12 @@ import java.sql.Statement;
 import java.sql.SQLException;
 import java.util.Base64;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+
 public class SQLDatabaseConnection {
 
 
@@ -33,31 +39,43 @@ public class SQLDatabaseConnection {
                         + "trustServerCertificate=false;"
                         + "loginTimeout=30;"
                         + "hostNameInCertificate=*.database.windows.net;";
-        System.out.println("Hello there!");
-        System.out.println(connectionUrl);
+
+        int retentionPeriod = Integer.parseInt(args[4]);
+        // System.out.println(connectionUrl);
         ResultSet resultSet = null;
 
         try (Connection connection = DriverManager.getConnection(connectionUrl);
                 Statement statement = connection.createStatement();) {
 
             // Create and execute a SELECT SQL statement.
-            String selectSql = " select top 10 id, conf from WF_JOBS ";
+
+            String selectSql = String.format("select id, conf from WF_JOBS  where created_time >= DATEADD(DAY, -%2d, SYSDATETIME())", retentionPeriod);
             resultSet = statement.executeQuery(selectSql);
 
             // Print results from select statement
             while (resultSet.next()) {
                 String hexString = resultSet.getString(2);
+                String jobId = resultSet.getString(1);
 
                 byte[] byteArray = hexStringToByteArray(hexString);
                 String base64EncodedString = Base64.getEncoder().encodeToString(byteArray);
 
-//                System.out.println("Base64 Encoded String: " + base64EncodedString);
 
-	       // Convert the base64 to xml
+                String decodedOutput = DecodeBase64(base64EncodedString);
 
-		String decodedOutput = DecodeBase64(base64EncodedString);
-		System.out.println("Decoded Base64  String: " + decodedOutput);
-//		
+                File directory = new File("oozie_jobs_output");
+                if (!directory.exists()) {
+                        directory.mkdir();
+                }
+                String fileName = directory.getAbsolutePath() + File.separator + jobId;
+                //String command = String.format(
+                    //"cat %s | sed '/<property>/{:a;N;/<\\/property>/!ba};/<name>end<\\/name>/d' | sed '/<property>/{:a;N;/<\\/property>/!ba};/<name>start<\\/name>/d' > %s.tmp",
+                    //fileName, fileName, fileName, fileName
+                //);
+//              String command = String.format("cp %s %s.tmp", fileName, fileName);
+                //System.out.println(fileName);
+                writeToFile(decodedOutput, fileName);
+//
             }
         }
 
@@ -76,18 +94,36 @@ public class SQLDatabaseConnection {
 
 
     public static String DecodeBase64(String base64String) throws Exception{
-	try {
-	     Configuration conf = new Configuration();
+        try {
+             Configuration conf = new Configuration();
              conf.set(CodecFactory.COMPRESSION_CODECS, "gz=" + GzipCompressionCodec.class.getName());
              conf.set(CodecFactory.COMPRESSION_OUTPUT_CODEC, GzipCompressionCodec.CODEC_NAME);
              CodecFactory.initialize(conf);
 
              String ucc = new StringBlob(Base64.getDecoder().decode(base64String)).getString();
-	    return ucc;
-	}catch (Exception e) {
+            return ucc;
+        }catch (Exception e) {
             throw new Exception("Error decoding Base64 string: " + e.getMessage(), e);
         }
     }
 
+    public static void executeCommand(String command) {
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            int exitCode = process.waitFor();
+            System.out.println("Command executed with exit code: " + exitCode);
+//        } catch (IOException | InterruptedException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-}   
+
+    public static void writeToFile(String content, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
